9c679352a082eae60d64bf8ca5b630bdd18700f5
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-02-27 15:34:17 -0500

    add comments

--- goaccess_view_logs.sh
+++ goaccess_view_logs.sh
@@ -4,6 +4,7 @@ apache_log_dir='/var/log/apache2'
 combined_log_files='goaccess_tmp.log'
 logfile_name='access.log'
 
+# find and combine all log files including ones compressed by gzip
 find "$apache_log_dir"/"$logfile_name"* -type f | xargs less  > $combined_log_files
 
 if [ $? -eq 0 ]
