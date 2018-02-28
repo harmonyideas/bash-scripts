92bf2042c450eacf14ea09356d497beef9249699
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-02-28 00:47:24 -0500

    add some comments

--- file_viewer.sh
+++ file_viewer.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Get a listing of all files in the current directory
+# and put them into array FILELIST. Use sed to remove ./
+# from filenames
+ 
 FILELIST=(`find ./ -maxdepth 1 -type f | sed 's/\.\///g'`)
+
+# Create prompt for user to select files and process 
+# input using select statement.  Find max length of 
+# array and add 1 to display choice to exit.  If no
+# file is found - display error message to user
+ 
 PS3="Select file to view or ($(expr ${#FILELIST[@]} + 1)) to exit: "
 
 select FILENAME in ${FILELIST[@]} "Exit Program"; 
