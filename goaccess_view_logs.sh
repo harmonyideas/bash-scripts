13aba7a61373b218a5c3058e25f79b9b01545665
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-02-28 01:02:41 -0500

    clean up

--- goaccess_view_logs.sh
+++ goaccess_view_logs.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 apache_log_dir='/var/log/apache2'
-combined_log_files='goaccess_tmp.log'
 logfile_name='access.log'
 
-# find and combine all log files including ones compressed by gzip
-find "$apache_log_dir"/"$logfile_name"* -type f | xargs less  > $combined_log_files
-
-if [ $? -eq 0 ]
-then
-    goaccess -f $combined_log_files
-    rm $combined_log_files
-fi
+zcat -f "$apache_log_dir"/"$logfile_name"* | goaccess
 
