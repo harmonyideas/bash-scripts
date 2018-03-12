71a189d6a804f649303149737a205587f8ff867a
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-03-12 01:30:02 -0400

    Cleanup of code - display rdiff-backup stats if successful

--- rdiff-run.sh
+++ rdiff-run.sh
@@ -9,12 +9,13 @@ LOCALDIR='/some/local/folder/' # directory on local host
 REMOTEHOST='remotehost' # name of remote host accessible by ssh
 USER='user' # remote ssh user
 
-if [ -x "/usr/bin/rdiff-backup"  ] && [ -d $LOCALDIR ]
+if [ -x "/usr/bin/rdiff-backup" ] && [ -d $LOCALDIR ]
 then
-    echo "$(/usr/bin/rdiff-backup --print-statistics -v$VERBOSE $USER@$REMOTEHOST::$REMOTEDIR $LOCALDIR)"
-    # Check return value of rdiff-backup. Send error message to syslog if failed.
-    [ "$?" != "0" ] && logger "$0: rdiff-backup of $REMOTEDIR has failed!"
+    $(/usr/bin/rdiff-backup -v${VERBOSE} ${USER}@${REMOTEHOST}::${REMOTEDIR} ${LOCALDIR})
+    [ "$?" -gt "0" ] && logger "$0: rdiff-backup of ${REMOTEDIR} has failed!" || 
+    rdiff-backup-statistics --begin-time `date '+%Y-%m-%d'` ${LOCALDIR} # Display backup stats
 else
-    echo "Command failed to run - please check rdiff-backup exists and $LOCALDIR is accessible!"
+    echo "Command failed to run - please check rdiff-backup exists and ${LOCALDIR} is accessible!"
 fi
+
 
