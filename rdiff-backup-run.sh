14683f74bad1bb4d691ac0b7fa4f6b7afa49c59e
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-03-12 15:57:16 -0400

    Add function to run rdiff-backup

--- rdiff-backup-run.sh
+++ rdiff-backup-run.sh
@@ -6,18 +6,23 @@
 verbose=1 # set verbose level
 host_dir='/some/remote/path' # directory on remote host
 local_dir='/some/local/path'
-host='myhost'
-backup_user='myuser'
 begin_time=$(date +%Y-%m-%d) # today's date
 error="$0: Backup of ${host_dir:-'target'} failed."
 success="$0: Backup of ${host_dir:-'target'} successful."
 options=("Run rdiff-backup" "Print Statistics")
-PS3="Select option or ($(expr ${#options[@]} + 1)) to exit: "
+PS3="Select option or ($(expr ${#options[@]} + 1)) to exit:"
 ### END INIT ###
 
+function run_backup() {
+    local host='myhost'
+    local backup_user='myuser'
+    $(/usr/bin/rdiff-backup -v"$verbose" \
+    "${backup_user}"@"${host}"::"${remote_dir}" "${local_dir}")
+}
+
+# Is the shell interactive? If not, display prompt
 if [ -n "$PS1" ]; then
-    $(/usr/bin/rdiff-backup -v"${verbose}" \
-    "${backup_user}"@$"{host}"::"${host_dir}" "${local_dir}")
+    run_backup
     [ "$?" -gt "0" ] && logger "${error}" || logger "${success}"
 else
 
@@ -25,16 +30,15 @@ select option in "${options[@]}" "Quit";
 do
     case $option in
         "Run rdiff-backup")
-            if [ -x "/usr/bin/rdiff-backup" ] && [ -d "$local_dir" ]; then
-                $(/usr/bin/rdiff-backup -v"${verbose}" \
-                "${backup_user}"@"${host}"::"${host_dir}" "${local_dir}")
+            if [ -x "/usr/bin/rdiff-backup" ] && [ -d "${local_dir}" ]; then
+                run_backup
                 [ "$?" -gt "0" ] && echo "${error}" || echo "${success}"
             else
                 echo "rdiff-backup not found or local directory inaccessible"
             fi
             ;;
         "Print Statistics")
-            rdiff-backup-statistics --begin-time "${begin_time}" "${local_dir}" # Display backup stats
+            rdiff-backup-statistics --begin-time "${begin_time}" "${local_dir}" 
             ;;
         "Quit") break ;;
         *) echo "Invalid option selected";;
