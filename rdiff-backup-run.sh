afe78ae6ba8de3599866f472db69fa71e5f3b320
harmonyideas <8549684+harmonyideas@users.noreply.github.com>
2018-03-12 21:59:15 -0400

    Add get_statistics function - cleanup

--- rdiff-backup-run.sh
+++ rdiff-backup-run.sh
@@ -1,48 +1,46 @@
-#!/usr/bin/env bash
-# A simple bash script for rdiff-backup.
-# requires ssh key based authentication 
-
 ### BEGIN INIT ###
-verbose=1 # set verbose level
-host_dir='/some/remote/path' # directory on remote host
-local_dir='/some/local/path'
-begin_time=$(date +%Y-%m-%d) # today's date
-error="$0: Backup of ${host_dir:-'target'} failed."
-success="$0: Backup of ${host_dir:-'target'} successful."
-options=("Run rdiff-backup" "Print Statistics")
-PS3="Select option or ($(expr ${#options[@]} + 1)) to exit:"
+host_dir='/path/remote/dir' # directory on remote host
+local_dir='/path/local/dir' # directory to store backups
+begin_time=$(date +%Y-%m-%d) # ignore logs before today's date
+menu_options=("Run rdiff-backup" "Print Statistics")
+PS3="Select option or ($(expr ${#menu_options[@]} + 1)) to exit: "
 ### END INIT ###
 
 run_backup() {
+    local cmd_options="-v1"
     local host='myhost'
     local backup_user='myuser'
-    $(/usr/bin/rdiff-backup -v"$verbose" \
-    "${backup_user}"@"${host}"::"${remote_dir}" "${local_dir}")
+    $(/usr/bin/rdiff-backup "${cmd_options}" \
+    "${backup_user}"@"${host}"::"${host_dir}" "${local_dir}")
 }
 
-# Is shell interactive? Yes:[display prompt] No:[run_backup]
+get_statistics() {
+    local stat_options="--begin-time ${begin_time} --quiet"
+    /usr/bin/rdiff-backup-statistics $stat_options "${local_dir}"
+}
+
 if [ -n "$PS1" ]; then
     run_backup
-    [ "$?" -gt "0" ] && logger "${error}" || logger "${success}"
+    [ "$?" -gt "0" ] && logger "Backup Failed." \
+    || logger "Backup was successful"
 else
 
-select option in "${options[@]}" "Quit";
+select option in "${menu_options[@]}" "Quit";
 do
     case $option in
         "Run rdiff-backup")
             if [ -x "/usr/bin/rdiff-backup" ] && [ -d "${local_dir}" ]; then
                 run_backup
-                [ "$?" -gt "0" ] && echo "${error}" || echo "${success}"
+                [ "$?" -gt "0" ] && echo "$0: Backup Failed." \
+                || echo "$0: Backup was successful."
             else
                 echo "rdiff-backup not found or local directory inaccessible"
             fi
             ;;
         "Print Statistics")
-            rdiff-backup-statistics --begin-time "${begin_time}" "${local_dir}"
+            get_statistics
             ;;
-        "Quit") 
-            break 
-            ;;
+        "Quit") break ;;
         *) echo "Invalid option selected";;
     esac
 done
